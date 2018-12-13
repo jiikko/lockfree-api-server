0ee06d168586714aa9d946eee6d3eb9225af2ac7
jiikko <n905i.1214@gmail.com>
2018-12-14 01:22:03 +0900

    モデルを追加する

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'active_support/testing/time_helpers'
+include ActiveSupport::Testing::TimeHelpers
+
+travel_to Time.new(2014, 5, 10, 0, 0, 0) do
+  ActiveRecord::Base.transaction do
+    100.times do
+      10.times do
+        OrderRequest.create(account_id: 1, body: '1')
+      end
+      10.times do
+        OrderRequest.create(account_id: 0, body: '2')
+      end
+    end
+  end
+end
+
